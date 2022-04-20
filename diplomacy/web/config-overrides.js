1958bdf77df5bc66629e18128778afba3133c55f
Joel Quiles <quilesbaker@gmail.com>
2022-04-19 20:38:25 -0400

    added new prod build task to analyze resulting bundle on localhost, separate than build script for deployment

--- diplomacy/web/config-overrides.js
+++ diplomacy/web/config-overrides.js
@@ -1,20 +1,14 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = function override(config, env) {
 
-    if (!isProd) {
-        
-        if (!config.plugins) {
-            config.plugins = [];
-        }
-
-        config.plugins.push(
-            new BundleAnalyzerPlugin()
-        );
+    if (!config.plugins) {
+        config.plugins = [];
     }
+    
+    config.plugins.push(
+        new BundleAnalyzerPlugin()
+    );
 
     return config;
-
 };
